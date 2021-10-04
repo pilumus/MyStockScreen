123506d8fcffb625efc1a8f43f2a9fdd4fb1af52
Сидоров Владимир Александрович <VASidorov@ibs.root.ibscorp.ru>
2021-10-04 12:10:46 +0300

    fs from mysql links added

---
+++ fs_from_mysql_links.py
@@ -0,0 +1,97 @@
+import mysql.connector
+from mysql.connector import Error
+from selenium import webdriver
+from bs4 import BeautifulSoup
+from config import db_config
+from math import floor as f
+from help_functions import fs_year_prep
+import time, os
+
+geck_path = os.path.join("C:\\", "Users", "VASidorov", "YandexDisk", "наука", "Коды", "geckodriver.exe")
+driver = webdriver.Firefox(executable_path=geck_path)
+
+
+def create_connection_mysql_db(db_host, user_name, user_password, db_name=None):
+    connection_db = None
+    try:
+        connection_db = mysql.connector.connect(
+            host=db_host,
+            user=user_name,
+            passwd=user_password,
+            database=db_name
+        )
+        print("Подключение к MySQL успешно выполнено")
+    except Error as db_connection_error:
+        print("Возникла ошибка: ", db_connection_error)
+    return connection_db
+
+
+connection = create_connection_mysql_db(db_config["mysql"]["host"],
+                                        db_config["mysql"]["user"],
+                                        db_config["mysql"]["pass"],
+                                        "corp_db")
+
+try:
+    cursor = connection.cursor()
+    select_links_by_country = '''
+        SELECT corp_id, url FROM corp_links
+        LIMIT 5;
+        '''
+    cursor.execute(select_links_by_country)
+    query_result = cursor.fetchall()
+
+    for link in query_result:
+        corp_parameters = []
+
+        driver.get(link[1] + "-financial-summary")
+        time.sleep(3)
+
+        try:
+            cheese = driver.find_element_by_class_name("toggleButton").click()
+        except:
+            print("No Annual click in {}".format(link[1]))
+        time.sleep(2)
+        pageSource = driver.page_source
+
+        soup = BeautifulSoup(pageSource, "html.parser")
+        table = soup.find("div", id="rsdiv")
+        # print(table)
+        # print(soup.find("th", text="Period Ending:"))
+        # print(soup.find("th", class_="arial_11 noBold title right period"))
+
+
+        for th in table.find("th", text="Period Ending:").parent.findAll("th", class_=""):
+            th = th.get_text()
+            corp_parameters.append(th)
+
+        for td in table.find("td", text="Total Revenue").parent.findAll("td", class_=""):
+            td = td.get_text()
+            td = f(float(td))
+            corp_parameters.append(td)
+
+        for td in table.find("td", text="Net Income").parent.findAll("td", class_=""):
+            td = td.get_text()
+            td = f(float(td))
+            corp_parameters.append(td)
+
+        for td in table.find("td", text="Total Assets").parent.findAll("td", class_=""):
+            td = td.get_text()
+            td = f(float(td))
+            corp_parameters.append(td)
+
+        for td in table.find("td", text="Total Equity").parent.findAll("td", class_=""):
+            td = td.get_text()
+            td = f(float(td))
+            corp_parameters.append(td)
+
+        corp_statements = fs_year_prep(corp_parameters)
+
+        for statement in corp_statements:
+            print (statement)
+
+except Error as error:
+    print(error)
+
+finally:
+    cursor.close()
+    connection.close()
--- help_functions.py
+++ help_functions.py
@@ -61,3 +61,27 @@ def market_cap_int(market_cap_str):
         return market_cap_str
         
     return mc_int
+
+
+# Перебираем полученные параметры и готовим список строк для записи в регистр
+# На вход список сырых параметров, на выход список списков
+
+def fs_year_prep(corp_parameters):
+
+    if len(corp_parameters) == 20:
+        j_max = 4
+    elif len(corp_parameters) == 15:
+        j_max = 3
+
+    corp_statements = []
+
+    for j in range(0, j_max):
+        i = j
+        fs_year = []
+        for cycle in range(0, 5):
+            fs_year.append(corp_parameters[i])
+            i = i + 4
+
+        corp_statements.append(fs_year)
+
+    return corp_statements
